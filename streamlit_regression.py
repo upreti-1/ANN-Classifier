05efa667f8c0b321635999cf1200d42e53eaeb05
upreti-1 <prashantupreti9844@gmail.com>
2026-04-25 23:05:40 +0530

    SalaryRegression using ANN with Streamlit

--- streamlit_regression.py
+++ streamlit_regression.py
@@ -3,12 +3,11 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 import pickle
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 
-# Loading the trained model
-model = tf.keras.models.load_model('model.h5')
+# Load model
+model = tf.keras.models.load_model('regression_model.h5')
 
-# Load the Encoder and Scaler
+# Load encoders & scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
@@ -19,26 +18,20 @@ with open('onehot_encoder_geo.pkl', 'rb') as file:
     onehot_encoder_geo = pickle.load(file)
 
 
+# ---------------- STREAMLIT UI ---------------- #
 
-# streamlit app
-st.title('Customer Churn Prediction')
+st.title('Estimated Salary Prediction')
 
 # ---- User Input ---- #
 
-# Geography (OneHotEncoder)
 geography = st.selectbox(
     'Geography',
     list(onehot_encoder_geo.categories_[0])
 )
 
-# Gender (LabelEncoder) - cleaner UI + safe encoding
-gender = st.selectbox(
-    'Gender',
-    ['Male', 'Female']  
-)
+gender = st.selectbox('Gender', ['Male', 'Female'])
 gender_encoded = label_encoder_gender.transform([gender])[0]
 
-# Numeric Inputs
 age = st.slider('Age', 18, 92, 30)
 
 balance = st.number_input(
@@ -55,50 +48,52 @@ credit_score = st.number_input(
     value=650
 )
 
-estimated_salary = st.number_input(
-    'Estimated Salary',
-    min_value=0.0,
-    step=1000.0,
-    format="%.2f"
-)
-
 tenure = st.slider('Tenure (Years)', 0, 10, 3)
 
 num_of_products = st.slider('Number of Products', 1, 4, 1)
 
-# Binary Inputs (Better UX with Yes/No)
 has_cr_card = st.selectbox('Has Credit Card', ['No', 'Yes'])
 has_cr_card = 1 if has_cr_card == 'Yes' else 0
 
 is_active_member = st.selectbox('Is Active Member', ['No', 'Yes'])
 is_active_member = 1 if is_active_member == 'Yes' else 0
 
+exited = st.selectbox("Exited", [0, 1])
+
+
+# ---------------- DATA PREPARATION ---------------- #
 
 input_data = pd.DataFrame({
-    "CreditScore" : [credit_score],
-    "Gender": [label_encoder_gender.transform([gender])[0]],
+    "CreditScore": [credit_score],
+    "Gender": [gender_encoded],
     "Age": [age],
     "Tenure": [tenure],
     "Balance": [balance],
     "NumOfProducts": [num_of_products],
     "HasCrCard": [has_cr_card],
     "IsActiveMember": [is_active_member],
-    "EstimatedSalary": [estimated_salary]
+    "Exited": [exited]
 })
+
+# One-hot encode Geography
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
-geo_encoded_df = pd.DataFrame(geo_encoded, columns= onehot_encoder_geo.get_feature_names_out(['Geography']))
+geo_columns = onehot_encoder_geo.get_feature_names_out(['Geography'])
 
-input_data = pd.concat([input_data.reset_index(drop= True), geo_encoded_df], axis = 1)
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=geo_columns)
 
+# Combine all features
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+
+# Match training columns
+input_data = input_data.reindex(columns=scaler.feature_names_in_, fill_value=0)
+
+# Scale input
 input_data_scaled = scaler.transform(input_data)
 
-# Prediction Churn
+
+# ---------------- PREDICTION ---------------- #
+
 prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
+predicted_salary = float(prediction[0][0])
 
-st.write('probability: ', prediction_proba)
-
-if prediction_proba > 0.5:
-    st.write('The Customer is likely to churn')
-else:
-    st.write("The Customer is not likely to churn")
+st.subheader(f'Estimated Salary: {predicted_salary:,.2f}')
